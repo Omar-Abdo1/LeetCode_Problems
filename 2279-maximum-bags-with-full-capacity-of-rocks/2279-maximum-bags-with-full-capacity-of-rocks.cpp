62599d2dd778336f0995437f6438c99a776a6531
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2022-12-27 21:36:13 +0200

    Time: 273 ms (66.01%), Space: 98.5 MB (5.27%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -9,21 +9,15 @@ public:
     int maximumBags(vector<int>& c, vector<int>& r, int k) {
         vector<oo>v;
         for(int i=0;i<c.size();++i)v.push_back({r[i],c[i],c[i]-r[i]});
+        int ans=0;
         sort(v.begin(),v.end());
      for(auto &[x,y,z] : v){
-         int need=z;
-         if(k>=need){
-             x+=need;
-             k-=need;
-         }
-         else{
-             x+=k;
-             break;
+         if(k>=z){
+             k-=z;
+             ans++;
          }
      }
-        int ans=0;
-        for(auto &[x,y,z] :v)ans+=x==y;
-        return ans;
-            
+     return ans;
+     
     }
 };
