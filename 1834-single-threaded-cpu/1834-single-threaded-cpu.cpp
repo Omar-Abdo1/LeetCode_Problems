b797b65afc4833f3fb47a6a534b161bd5998be9c
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2022-12-30 01:19:40 +0200

    Time: 500 ms (99.45%), Space: 137 MB (39.89%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -13,34 +13,33 @@ public:
         vector<int>ans;
         int i=0;
         for(auto &x : tasks )v.push_back({x[0],x[1],i++});
-        sort(v.begin(),v.end());
+        sort(v.begin(),v.end());    
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-         i=0;
-        long long time=0;
-        while(i<v.size()){
-         
-            while(i<v.size() && time>=v[i].st){
+           long long time=0;
+            i=0;
+           while(i<v.size()){
+          
+           while(i<v.size() && time>=v[i].st){
                 pq.push({v[i].take,v[i].i});
-                ++i;
+                i++;
             }
-            if(pq.size()){
-                auto [x,y]=pq.top();
-                pq.pop();
-                ans.push_back(y);
-                time+=x;
+            if(pq.empty()){
+               pq.push({v[i].take,v[i].i});
+               time=v[i].st;
+               i++;
             }
-            else{
-                // choose the next start postion 
-                ans.push_back(v[i].i);
-                time=v[i].st+v[i].take;
-                ++i;
-            }
-        }
-        while(!pq.empty()){
             auto [x,y]=pq.top();
-            ans.push_back(y);
             pq.pop();
-        }
+            ans.push_back(y);
+            time+=x;
+           }
+        
+           while(!pq.empty()){
+                  auto [x,y]=pq.top();
+            pq.pop();
+            ans.push_back(y);
+           }
+           
     
        return ans;
     }
