d5bbbd9b62778f99cc89882227e7e6468ff4a205
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2023-01-01 02:56:53 +0200

    Time: 174 ms (99.62%), Space: 79.6 MB (97.92%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    const int N=1e6+1;
-    int mp[1000007];
-    bool can(int mid,vector<int>&v){
-         memset(mp,0,sizeof mp);
-        int p=0;
-     for(int i=0;i<mid;++i){
-         mp[v[i]]++;
-         if(mp[v[i]]==2)p++;
-     }
-     for(int i=mid,j=0;i<v.size();++i && j++){
-         if(p>=1)return 1;
-         mp[v[j]]--;
-         if(mp[v[j]]==1)p--;
-         mp[v[i]]++;
-         if(mp[v[i]]==2)p++;
-     }
-        return p>=1;
-        
-    }
+            int mp[1000007];
+
     int minimumCardPickup(vector<int>& v) {
-      
-        int l=1,r=v.size(),mid,ans=-1;
-        while(l<=r){
-            mid=(l+r)/2;
-            can(mid,v) ? r=mid-1,ans=mid :l=mid+1; 
-        }
+      int ans=1e9;
+      for(int i=0;i<v.size();++i){
+          if(mp[v[i]]){
+              ans=min(ans,i-mp[v[i]]+2);
+          }
+          mp[v[i]]=i+1;
+      }
+        if(ans==1e9)ans=-1;
         return ans;
-         
         
         
     }
