045f38f7c3517f3231d49ac7f6f1ed1709de9f66
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2023-01-12 15:33:55 +0200

    Time: 459 ms (97.04%), Space: 157.3 MB (100.00%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     /// count from down to top 
-    vector<int>cn;
+    vector<int>cn,in,out,fr;
     vector<vector<int>>v;
     string s;
-    vector<int> dfs(int i, int p){
-        vector<int>fr(27);
-        fr[s[i]-'a']++;
-        for(auto &j :v[i])if(j!=p){
-            auto me = dfs(j,i);
-            cn[i]+=me[s[i]-'a']; // (i) will increase by what the bottom substree from the same lable then (i) will return to (p) a vector which contains the frequncy until node (i)
-            for(int k=0;k<26;++k)fr[k]+=me[k];
-        }
-        cn[i]=fr[s[i]-'a'];
-        return fr;
+     void dfs(int i, int p){
+     in[i]=fr[s[i]-'a']++;
+      
+     for(auto &j :v[i])if(j!=p){
+         dfs(j,i);
+     }
+         
+      cn[i]=fr[s[i]-'a']-in[i];   
     }
     
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string t) {
-        cn=vector<int>(n);
+        in=out=cn=vector<int>(n);
         v=vector<vector<int>>(n);
+        fr=vector<int>(26);
         s=t;
         for(auto &x : edges){
             v[x[0]].push_back(x[1]);
             v[x[1]].push_back(x[0]);
         }
-        auto me =dfs(0,0);
+        dfs(0,0);
         return cn;
     }
 };
