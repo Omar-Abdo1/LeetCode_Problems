c12a010f07024d80d5da803b7b313582dc7eb17a
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2023-01-12 15:26:04 +0200

    Time: 624 ms (79.26%), Space: 217.6 MB (54.08%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -9,7 +9,7 @@ public:
         fr[s[i]-'a']++;
         for(auto &j :v[i])if(j!=p){
             auto me = dfs(j,i);
-            cn[i]+=me[s[i]-'a'];
+            cn[i]+=me[s[i]-'a']; // (i) will increase by what the bottom substree from the same lable then (i) will return to (p) a vector which contains the frequncy until node (i)
             for(int k=0;k<26;++k)fr[k]+=me[k];
         }
         cn[i]=fr[s[i]-'a'];
@@ -17,7 +17,7 @@ public:
     }
     
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string t) {
-        cn=vector<int>(n,1);
+        cn=vector<int>(n);
         v=vector<vector<int>>(n);
         s=t;
         for(auto &x : edges){
