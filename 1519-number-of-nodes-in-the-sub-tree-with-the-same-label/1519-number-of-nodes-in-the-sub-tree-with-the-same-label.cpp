7ee7c72ef7649e8cde7cd76e9e75e7c40990f978
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2023-01-12 15:21:10 +0200

    Time: 606 ms (82.22%), Space: 217.7 MB (54.08%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    /// count from down to top 
+    vector<int>cn;
+    vector<vector<int>>v;
+    string s;
+    vector<int> dfs(int i, int p){
+        vector<int>fr(27);
+        fr[s[i]-'a']++;
+        for(auto &j :v[i])if(j!=p){
+            auto me = dfs(j,i);
+            cn[i]+=me[s[i]-'a'];
+            for(int k=0;k<26;++k)fr[k]+=me[k];
+        }
+        cn[i]=fr[s[i]-'a'];
+        return fr;
+    }
+    
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string t) {
+        cn=vector<int>(n,1);
+        v=vector<vector<int>>(n);
+        s=t;
+        for(auto &x : edges){
+            v[x[0]].push_back(x[1]);
+            v[x[1]].push_back(x[0]);
+        }
+        auto me =dfs(0,0);
+        return cn;
+    }
+};
