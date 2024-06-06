fae3430a9164e52e1c0ae142876ac4ed026421e7
OmarAbdo <113479845+Omar-Abdo1@users.noreply.github.com>
2024-06-06 13:01:05 +0300

    Time: 63 ms (47.40%), Space: 31.5 MB (33.43%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -13,8 +13,7 @@ public:
             int cur = mp.begin()->first;
             for(int i=0;i<k;++i){
                 if(mp[cur+i]==0) return 0;
-                --mp[cur+i];
-                if(mp[cur+i]==0)mp.erase(cur+i);
+                if(--mp[cur+i]==0)mp.erase(cur+i);
             }
     
         }
